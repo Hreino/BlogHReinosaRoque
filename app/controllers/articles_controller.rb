34ed6ff6c85e85f55036854bdd47d249455a17a8
Hreinosa <hreinosa@hotmail.com>
2018-11-14 22:00:19 -0600

    crud articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,11 +7,57 @@ class ArticlesController < ApplicationController
      end
 
      def show
+
           @article = Article.find(params[:id])
      end
 
      def new
           @article = Article.new
      end
+
+     def create
+          @article = Article.new(article_params)
+          if @article.save
+            flash[:success] = "Articulo publicado exitosamente"
+            redirect_to @article
+          else
+            flash[:error] = "Algo salio mal, el post no fue publicado"
+            render 'new'
+          end
+     end
+
+     def destroy
+          @article = Article.find(params[:id])
+          if @article.destroy #elimina el objeto de la base de datos
+               flash[:success] = 'Article was successfully deleted.'
+               redirect_to articles_url
+          else
+               flash[:error] = 'Something went wrong'
+               redirect_to articles_url
+          end
+     end
+     def edit
+          @article = Article.find(params[:id])
+     end
+     
+
+     def update
+          @article = Article.find(params[:id])
+         if @article.update_attributes(article_params)
+           flash[:success] = "Article was successfully updated"
+           redirect_to @article
+         else
+           flash[:error] = "Something went wrong"
+           render 'edit'
+         end
+     end
+     
+     #Strong params
+     private
+     def article_params
+          #permit quiere decir que solo esos parametros se pueden recibir en el controlador
+          params.require(:article).permit(:title, :content)
+     end
+     
      
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
-     
+    validates :title, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 20}
+    validates :content, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 50}
+    
+    
 end
